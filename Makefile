f41a1d686a74c2c953bd8280fda4c33f55b45156
YoavPolak <yoavpolakovsky@gmail.com>
2024-08-25 12:48:29 +0300

    Final.

--- Makefile
+++ Makefile
@@ -1,29 +1,51 @@
+# Define the target executable name
 APP=tc
 
+# Declare $(APP) as a phony target to avoid conflicts with files named "tc"
 .PHONY: $(APP)
+# Rule to build the $(APP) executable
 $(APP): skel
+	@# Compile the tc.c source file into the $(APP) executable, linking against libbpf and libelf
 	clang tc.c -Wno-unsequenced -lbpf -lelf -o $(APP)
 
+# Declare vmlinux as a phony target
 .PHONY: vmlinux
+# Rule to generate BTF (BTF Type Information) from the vmlinux file
 vmlinux:
+	@# Dump the BTF information from the kernel vmlinux file into vmlinux.h in C format
 	bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
 
+# Declare bpf as a phony target
 .PHONY: bpf
+# Rule to compile the BPF program
 bpf: vmlinux
+	@# Compile the BPF program tc.bpf.c to an object file tc.bpf.o with debug info and optimization
 	clang -g -O2 -target bpf -D__TARGET_ARCH_x86_64 -c tc.bpf.c -o tc.bpf.o
 
+# Declare skel as a phony target
 .PHONY: skel
+# Rule to generate BPF skeleton code
 skel: bpf
+	@# Generate the BPF skeleton header file tc.skel.h from the BPF object file tc.bpf.o
 	bpftool gen skeleton tc.bpf.o name tc > tc.skel.h
 
+# Declare run as a phony target
 .PHONY: run
+# Rule to run the $(APP) executable with specified arguments
 run: $(APP)
+	@# Execute the $(APP) executable with example arguments as root
 	sudo ./$(APP) 53 5355 80 443 22 50005 50007 50008
 
+# Declare block as a phony target
 .PHONY: block
+# Rule to block the network traffic using $(APP)
 block: $(APP)
+	@# Execute the $(APP) executable to block network traffic
 	sudo ./$(APP)
 
+# Declare clean as a phony target
 .PHONY: clean
+# Rule to remove all generated files
 clean:
-	-rm -rf *.o *.skel.h vmlinux.h $(APP)
+	@# Remove all object files, skeleton header files, BTF header file, and the $(APP) executable
+	rm -rf *.o *.skel.h vmlinux.h $(APP)
--- tc.h
+++ tc.h
@@ -1,42 +1,49 @@
 #ifndef __TC_H__
 #define __TC_H__
 
+// Define the maximum length of task names (e.g., process names)
 #define TASK_LEN 16
 
+// Enumeration for different IP protocols
 enum ip_proto {
-  UDP_V6,
-	TCP_V4,
-	TCP_V6,
-	UDP_V4,
-	
+    UDP_V6, // UDP over IPv6
+    TCP_V4, // TCP over IPv4
+    TCP_V6, // TCP over IPv6
+    UDP_V4, // UDP over IPv4
 };
 
+// Enumeration for packet state
 enum pkt_state {
-	BLOCKED,
-	ALLOWED,
+    BLOCKED, // Packet is blocked
+    ALLOWED, // Packet is allowed
 };
 
-
+// Structure to hold IP address and port information
 struct ip_info {
-	enum ip_proto ipp;
-	union {
-		uint8_t ipv6_daddr[16];
-		uint8_t ipv4_daddr[4];
-	} addr;
-	uint16_t port;
+    enum ip_proto ipp; // Protocol type (UDP/TCP) and IP version (IPv4/IPv6)
+    
+    // Union to accommodate either IPv4 or IPv6 address
+    union {
+        uint8_t ipv6_daddr[16]; // IPv6 address (16 bytes)
+        uint8_t ipv4_daddr[4];  // IPv4 address (4 bytes)
+    } addr;
+    
+    uint16_t port; // Port number
 };
 
+// Structure for holding information about network events
 struct tc_evt {
-	enum pkt_state pkt_state;
-	pid_t tgid;
-	pid_t pid;
-	char comm[TASK_LEN];
-	uint16_t eth_type;
+    enum pkt_state pkt_state; // State of the packet (BLOCKED/ALLOWED)
+    pid_t tgid;               // Thread group ID (usually the process ID)
+    pid_t pid;                // Process ID
+    char comm[TASK_LEN];      // Command name of the process (up to TASK_LEN characters)
+    uint16_t eth_type;        // Ethernet type (for example, to distinguish between IPv4 and IPv6)
 
-	union {
-		struct ip_info ip;
-		struct arphdr arp;
-	};
+    // Union to accommodate either IP information or ARP header
+    union {
+        struct ip_info ip;  // IP information (for IP packets)
+        struct arphdr arp; // ARP header (for ARP packets)
+    };
 };
 
-#endif //__TC_H__
+#endif // __TC_H__
