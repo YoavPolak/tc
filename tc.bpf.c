35b84d72d04db9395bd7f747907cd3808ea28ab6
YoavPolak <yoavpolakovsky@gmail.com>
2024-08-25 12:41:37 +0300

    Commented/Final product

--- tc.bpf.c
+++ tc.bpf.c
@@ -5,27 +5,29 @@
 
 #include "tc.h"
 
-#define ETH_ALEN 6
-#define ETH_P_IP 0x0800
-#define ETH_P_IPV6 0X86DD
-#define ETH_P_ARP 0x0806
+#define ETH_ALEN 6  // Length of the Ethernet address
+#define ETH_P_IP 0x0800  // Ethernet protocol type for IPv4
+#define ETH_P_IPV6 0X86DD  // Ethernet protocol type for IPv6
+#define ETH_P_ARP 0x0806  // Ethernet protocol type for ARP
 
-//#include <linux/pkt_cls.h>
-#define TC_ACT_UNSPEC	(-1)
-#define TC_ACT_OK			0
-#define TC_ACT_RECLASSIFY	1
-#define TC_ACT_SHOT			2
-#define TC_ACT_PIPE			3
-#define TC_ACT_STOLEN		4
-#define TC_ACT_QUEUED		5
-#define TC_ACT_REPEAT		6
-#define TC_ACT_REDIRECT		7
-#define TC_ACT_TRAP			8
+// Define possible actions for Traffic Control (TC) in BPF
+#define TC_ACT_UNSPEC   (-1)
+#define TC_ACT_OK       0
+#define TC_ACT_RECLASSIFY 1
+#define TC_ACT_SHOT     2
+#define TC_ACT_PIPE     3
+#define TC_ACT_STOLEN   4
+#define TC_ACT_QUEUED   5
+#define TC_ACT_REPEAT   6
+#define TC_ACT_REDIRECT 7
+#define TC_ACT_TRAP     8
 
+// Define byte-order conversion macros
 #define ntohs bpf_ntohs
 #define ntohl bpf_ntohl
 
-//bpf maps here: ports, ringbuffer
+// Define BPF maps here: ports, ringbuffer
+// Map to store allowed ports
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __uint(max_entries, 10);
@@ -33,234 +35,201 @@ struct {
     __type(key, u32);
 } ports SEC(".maps");
 
-struct
-{
-  __uint (type, BPF_MAP_TYPE_RINGBUF);
-  __uint (max_entries, 32 * 1024);
+// Ring buffer for event logging
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 32 * 1024); // Size of the ring buffer
 } rb SEC (".maps");
 
+pid_t my_pid = 0; // PID of the process to be excluded from packet reporting
 
-pid_t my_pid = 0;
-
-//distinguish between the ip protocols
-
-
+// Function to determine if the Ethernet frame is IPv4
 struct iphdr* is_ipv4(struct ethhdr *eth, void *data_end)
 {
-	struct iphdr *iph = NULL;
-	if (!eth || !data_end) return NULL;
-	if((void*)eth + sizeof(*eth) + sizeof(*iph) > data_end) return NULL;
+    struct iphdr *iph = NULL;
+    if (!eth || !data_end) return NULL;
+    if((void*)eth + sizeof(*eth) + sizeof(*iph) > data_end) return NULL;
 
-	if(eth->h_proto == bpf_htons(ETH_P_IP)) { //this lines checks if it is indeed ipv4
-		iph = (struct iphdr*)((void*)eth + sizeof(*eth));
-	}
-	return iph;
+    if(eth->h_proto == bpf_htons(ETH_P_IP)) { // Check if protocol is IPv4
+        iph = (struct iphdr*)((void*)eth + sizeof(*eth));
+    }
+    return iph;
 }
 
+// Function to determine if the Ethernet frame is IPv6
 struct ipv6hdr* is_ipv6(struct ethhdr *eth, void *data_end)
 {
-	struct ipv6hdr *iph = NULL;
-	if (!eth || !data_end) return NULL;
-	if((void*)eth + sizeof(*eth) + sizeof(*iph) > data_end) return NULL;
+    struct ipv6hdr *iph = NULL;
+    if (!eth || !data_end) return NULL;
+    if((void*)eth + sizeof(*eth) + sizeof(*iph) > data_end) return NULL;
 
-	if(eth->h_proto == bpf_htons(ETH_P_IPV6)) { //this lines checks if it is indeed ipv6
-		iph = (struct ipv6hdr*)((void*)eth + sizeof(*eth));
-	}
-	return iph;
+    if(eth->h_proto == bpf_htons(ETH_P_IPV6)) { // Check if protocol is IPv6
+        iph = (struct ipv6hdr*)((void*)eth + sizeof(*eth));
+    }
+    return iph;
 }
 
+// Function to determine if the Ethernet frame is ARP
 struct arphdr* is_arp(struct ethhdr *eth, void *data_end)
 {
-	struct arphdr *arp = NULL;
-	if (!eth || !data_end) return NULL;
-	if((void*)eth + sizeof(*eth) + sizeof(*arp) > data_end) return NULL;
+    struct arphdr *arp = NULL;
+    if (!eth || !data_end) return NULL;
+    if((void*)eth + sizeof(*eth) + sizeof(*arp) > data_end) return NULL;
 
-	if(eth->h_proto == bpf_htons(ETH_P_ARP)) { //this lines checks if it is indeed arp 
-		arp = (struct arphdr*)((void*)eth + sizeof(*eth));
-	}
-	return arp;
+    if(eth->h_proto == bpf_htons(ETH_P_ARP)) { // Check if protocol is ARP
+        arp = (struct arphdr*)((void*)eth + sizeof(*eth));
+    }
+    return arp;
 }
 
-
-struct udphdr* is_udp(void *iph, u8 hdr_sz, void* data_end)
-{
-	struct udphdr *udph = NULL;
-	if (!iph || !data_end) return NULL;
-	if((void*)((uint8_t*)iph + hdr_sz + sizeof(*udph)) > data_end) return NULL;
-	
-	int proto = -1;
-	if(hdr_sz == sizeof(struct iphdr)) {
-		struct iphdr *v4 = (struct iphdr*)iph;
-		proto = v4->protocol;
-	} else if (hdr_sz == sizeof(struct ipv6hdr)) {
-		struct ipv6hdr *v6 = (struct ipv6hdr*)iph;
-		proto = v6->nexthdr;
-	}
-
-	if(proto == IPPROTO_UDP) {
-		udph = (struct udphdr*)((uint8_t*)iph + hdr_sz);
-	}
-	return udph;
-}
-
-struct tcphdr* is_tcp(void *iph, u8 hdr_sz, void* data_end)
-{
-	struct tcphdr *tcph = NULL;
-	if (!iph || !data_end) return NULL;
-	if((void*)((uint8_t*)iph + hdr_sz + sizeof(*tcph)) > data_end) return NULL; //checks if the size is bigger
-	
-	int proto = -1;
-	if(hdr_sz == sizeof(struct iphdr)) {
-		struct iphdr *v4 = (struct iphdr*)iph;
-		proto = v4->protocol;
-	} else if (hdr_sz == sizeof(struct ipv6hdr)) {
-		struct ipv6hdr *v6 = (struct ipv6hdr*)iph;
-		proto = v6->nexthdr;
-	}
-
-	if(proto == IPPROTO_TCP) {
-		tcph = (struct tcphdr*)((uint8_t*)iph + hdr_sz);
-	}
-	return tcph;
-}
-
-
-
-
+// BPF program for packet classification
 SEC("classifier")
-int handle_egress( struct __sk_buff *skb)
-{	
-	int rc = TC_ACT_SHOT;
-	struct task_struct *t = (struct task_struct*)bpf_get_current_task();
-	pid_t pid = BPF_CORE_READ(t, pid);
-	pid_t tgid = BPF_CORE_READ(t, tgid);
-	if (tgid == my_pid) {
-		rc = TC_ACT_OK; // dont report the kernel dont report myself
-		bpf_printk("dont report me!");
-		return rc;
-	}
+int handle_egress(struct __sk_buff *skb)
+{
+    int rc = TC_ACT_SHOT; // Default action is to drop the packet
+    struct task_struct *t = (struct task_struct*)bpf_get_current_task();
+    pid_t pid = BPF_CORE_READ(t, pid);
+    pid_t tgid = BPF_CORE_READ(t, tgid);
 
-	//reads the data from the socket buffer
-	void *data_end = (void *)(unsigned long long)skb->data_end;
-  void *data = (void*)(unsigned long long)skb->data;
-  struct ethhdr *eth = data;
-  if((void*)(eth+1) > data_end) goto err;
-	struct iphdr *iph= is_ipv4(eth, data_end);
-	struct ipv6hdr *iph6 = is_ipv6(eth, data_end);
-	struct arphdr *arp = is_arp(eth, data_end);
-
-	struct tc_evt *evt = NULL;
-	evt = bpf_ringbuf_reserve(&rb, sizeof(*evt), 0);
-	if (!evt) {
-		bpf_printk("no ringbuff");
-		goto rb_err;
-	}
-
-	__builtin_memset(evt, 0, sizeof(*evt));
-	evt->eth_type = bpf_htons(BPF_CORE_READ(eth, h_proto));
-	bpf_probe_read_kernel_str(evt->comm, TASK_LEN, BPF_CORE_READ(t, group_leader, comm));
-	evt->pid = pid;
-	evt->tgid=tgid;
-	bpf_printk("comm is: %s eth_type is 0x%04x", evt->comm,  evt->eth_type);
-
-
-	if(iph) {
-	  //maybe to change so it will be in a function	
-    struct tcphdr *tcph = NULL;
-    struct udphdr *udph = NULL;
-
-    if(iph->protocol == IPPROTO_UDP) {
-      udph = (void*)(iph + 1);
-      if ((void*)(udph + 1) > data_end) goto err;
-      bpf_printk("is udp %d", ntohs(udph->source)); 
-			bpf_printk("  %d\n", ntohs(udph->dest));
-
-      evt->ip.ipp = UDP_V4;
-      evt->ip.port = ntohs(udph->dest);
-    } else if (iph->protocol == IPPROTO_TCP) {
-      tcph = (void*)(iph+1);
-      if((void*)(tcph + 1) > data_end) goto err;
-      bpf_printk("\nis tcp %d\n", ntohs(tcph->source));
-		  bpf_printk(" %d\n", ntohs(tcph->dest));
-
-      evt->ip.ipp = TCP_V4;
-      evt->ip.port = ntohs(tcph->dest);
+    // Skip processing if the packet is from the current process
+    if (tgid == my_pid) {
+        rc = TC_ACT_OK; // Allow the packet
+        bpf_printk("don't report me!"); // Log message for debugging
+        return rc;
     }
 
-		u32 daddr = iph->daddr;
-		if(tcph || udph) {
-			bpf_printk("dest ip is %08x", ntohl(daddr));
-		} else {
-			goto err;
-		}
+    // Read packet data from the socket buffer
+    void *data_end = (void *)(unsigned long long)skb->data_end;
+    void *data = (void*)(unsigned long long)skb->data;
+    struct ethhdr *eth = data;
 
+    // Check if there is enough data for Ethernet header
+    if((void*)(eth+1) > data_end) goto err;
 
-		bpf_probe_read_kernel(&evt->ip.addr.ipv4_daddr, sizeof(evt->ip.addr.ipv4_daddr), &daddr);
-    
+    // Identify protocol type
+    struct iphdr *iph = is_ipv4(eth, data_end);
+    struct ipv6hdr *iph6 = is_ipv6(eth, data_end);
+    struct arphdr *arp = is_arp(eth, data_end);
 
-  } else if (iph6) {
+    // Reserve space in the ring buffer for event logging
+    struct tc_evt *evt = NULL;
+    evt = bpf_ringbuf_reserve(&rb, sizeof(*evt), 0);
+    if (!evt) {
+        bpf_printk("no ringbuff"); // Log message if ring buffer is full
+        goto rb_err;
+    }
 
-		struct udphdr *udph = NULL;
-		struct tcphdr *tcph = NULL;
-	
-    if(iph6->nexthdr == IPPROTO_UDP) {
-      udph = (void*)(iph6 + 1);
-      if ((void*)(udph + 1) > data_end) goto err;
+    __builtin_memset(evt, 0, sizeof(*evt)); // Initialize the event structure
+    evt->eth_type = bpf_htons(BPF_CORE_READ(eth, h_proto)); // Store Ethernet type
+    bpf_probe_read_kernel_str(evt->comm, TASK_LEN, BPF_CORE_READ(t, group_leader, comm)); // Store process name
+    evt->pid = pid;
+    evt->tgid = tgid;
+    bpf_printk("comm is: %s eth_type is 0x%04x", evt->comm,  evt->eth_type);
 
-      bpf_printk("is udp %d", ntohs(udph->source)); 
-			bpf_printk("  %d\n", ntohs(udph->dest));
+    if(iph) {
+        struct tcphdr *tcph = NULL;
+        struct udphdr *udph = NULL;
 
-      evt->ip.ipp = UDP_V4;
-      evt->ip.port = ntohs(udph->dest);
-    } else if (iph6->nexthdr == IPPROTO_TCP) {
-      tcph = (void*)(iph6+1);
-      if((void*)(tcph + 1) > data_end) goto err;
+        if(iph->protocol == IPPROTO_UDP) { // Handle UDP packets
+            udph = (void*)(iph + 1);
+            if ((void*)(udph + 1) > data_end) goto err;
+            bpf_printk("is udp %d", ntohs(udph->source)); 
+            bpf_printk("  %d\n", ntohs(udph->dest));
 
-      bpf_printk("\nis tcp %d\n", ntohs(tcph->source));
-		  bpf_printk(" %d\n", ntohs(tcph->dest));
+            evt->ip.ipp = UDP_V4;
+            evt->ip.port = ntohs(udph->dest);
+        } else if (iph->protocol == IPPROTO_TCP) { // Handle TCP packets
+            tcph = (void*)(iph + 1);
+            if((void*)(tcph + 1) > data_end) goto err;
+            bpf_printk("\nis tcp %d\n", ntohs(tcph->source));
+            bpf_printk(" %d\n", ntohs(tcph->dest));
 
-      evt->ip.ipp = TCP_V4;
-      evt->ip.port = ntohs(tcph->dest);
-    } else goto err;
+            evt->ip.ipp = TCP_V4;
+            evt->ip.port = ntohs(tcph->dest);
+        }
 
-		if(tcph || udph) {
-			//TODO
-		} else {
-			goto err;
-		}
+        u32 daddr = iph->daddr;
+        if(tcph || udph) {
+            bpf_printk("dest ip is %08x", ntohl(daddr));
+        } else {
+            goto err;
+        }
 
-		bpf_probe_read_kernel(&evt->ip.addr.ipv6_daddr, sizeof(evt->ip.addr.ipv6_daddr), &iph6->daddr.in6_u.u6_addr8);
+        bpf_probe_read_kernel(&evt->ip.addr.ipv4_daddr, sizeof(evt->ip.addr.ipv4_daddr), &daddr);
 
-	} else if (arp) {
-		bpf_probe_read_kernel(&evt->arp, sizeof(evt->arp), arp);
-		bpf_printk("arp");
-	}
+    } else if (iph6) {
+        struct udphdr *udph = NULL;
+        struct tcphdr *tcph = NULL;
 
-  rc = TC_ACT_SHOT;
-	if(evt->eth_type == ETH_P_ARP) rc = TC_ACT_OK;
-	bpf_printk("%d\n", evt->ip.port);
+        if(iph6->nexthdr == IPPROTO_UDP) { // Handle UDP packets for IPv6
+            udph = (void*)(iph6 + 1);
+            if ((void*)(udph + 1) > data_end) goto err;
 
-	u32 i = 0;
-	for(i=0;i<10;i++) {
-		u16 *port;
-		u32 key = i;
-		port = bpf_map_lookup_elem(&ports, &key);
-		if(port && evt->ip.port == *port && *port != 0) { //i need to remeber to maybe remove the last statement
-			rc = TC_ACT_OK;
-		}
-	}
+            bpf_printk("is udp %d", ntohs(udph->source)); 
+            bpf_printk("  %d\n", ntohs(udph->dest));
 
-	if(rc == TC_ACT_SHOT) {
-		evt->pkt_state = BLOCKED;
-	} else {
-		evt->pkt_state = ALLOWED;
-	}
-	bpf_ringbuf_submit(evt, 0);
-	evt = NULL;
+            evt->ip.ipp = UDP_V4;
+            evt->ip.port = ntohs(udph->dest);
+        } else if (iph6->nexthdr == IPPROTO_TCP) { // Handle TCP packets for IPv6
+            tcph = (void*)(iph6 + 1);
+            if((void*)(tcph + 1) > data_end) goto err;
+
+            bpf_printk("\nis tcp %d\n", ntohs(tcph->source));
+            bpf_printk(" %d\n", ntohs(tcph->dest));
+
+            evt->ip.ipp = TCP_V4;
+            evt->ip.port = ntohs(tcph->dest);
+        } else {
+            goto err;
+        }
+
+        if(tcph || udph) {
+            // Extract and log the destination IPv6 address
+            __be32 daddr_high = iph6->daddr.in6_u.u6_addr32[0];
+            __be32 daddr_mid = iph6->daddr.in6_u.u6_addr32[1];
+            __be32 daddr_low = iph6->daddr.in6_u.u6_addr32[2];
+            __be32 daddr_last = iph6->daddr.in6_u.u6_addr32[3];
+
+            bpf_printk("dest ip is %08x:%08x:%08x:%08x\n", ntohl(daddr_high), ntohl(daddr_mid), ntohl(daddr_low), ntohl(daddr_last));
+        } else {
+            goto err;
+        }
+
+        bpf_probe_read_kernel(&evt->ip.addr.ipv6_daddr, sizeof(evt->ip.addr.ipv6_daddr), &iph6->daddr.in6_u.u6_addr8);
+
+    } else if (arp) { // Handle ARP packets
+        bpf_probe_read_kernel(&evt->arp, sizeof(evt->arp), arp);
+        bpf_printk("arp");
+    }
+
+    rc = TC_ACT_SHOT; // Default to dropping the packet
+    if(evt->eth_type == ETH_P_ARP) rc = TC_ACT_OK; // Allow ARP packets
+    bpf_printk("%d\n", evt->ip.port);
+
+    // Check if the packet's destination port is in the allowed list
+    u32 i = 0;
+    for(i = 0; i < 10; i++) {
+        u16 *port;
+        u32 key = i;
+        port = bpf_map_lookup_elem(&ports, &key);
+        if(port && evt->ip.port == *port && *port != 0) {
+            rc = TC_ACT_OK; // Allow packets to/from allowed ports
+        }
+    }
+
+    if(rc == TC_ACT_SHOT) {
+        evt->pkt_state = BLOCKED; // Mark the packet as blocked
+    } else {
+        evt->pkt_state = ALLOWED; // Mark the packet as allowed
+    }
+    bpf_ringbuf_submit(evt, 0); // Submit the event to the ring buffer
+    evt = NULL;
+
 err:
-	if (evt) bpf_ringbuf_discard(evt, 0);
+    if (evt) bpf_ringbuf_discard(evt, 0); // Discard the event if not submitted
 rb_err:
-	return rc;
+    return rc; // Return the action to take on the packet
 }
 
+// License declaration
 char LICENSE[] SEC("license") = "GPL";
--- tc.c
+++ tc.c
@@ -10,31 +10,75 @@
 #include <arpa/inet.h>
 #include <bpf/bpf.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "tc.h"
 #include "tc.skel.h"
 
-#define LO_IFINDEX 2
+#define LO_IFINDEX 2  // Index of the loopback interface
+#define BUFFER_SIZE 256
 
-static volatile bool exiting = false;
+static volatile bool exiting = false;  // Flag to signal exit
 
-//return the ip protocol
+// Function to return the string representation of the IP protocol
 char* print_proto(enum ip_proto ipp)
 {
 	switch(ipp) {
     default:
-      return "OTHER";
+      return "OTHER";  // Default case for unknown protocols
 		case TCP_V4:
-			return "TCP ipv4";
+			return "TCP ipv4";  // IPv4 TCP protocol
 		case TCP_V6:
-			return "TCP ipv6";
+			return "TCP ipv6";  // IPv6 TCP protocol
 		case UDP_V4:
-			return "UDP ipv4";
+			return "UDP ipv4";  // IPv4 UDP protocol
 		case UDP_V6:
-			return "UDP ipv6";
+			return "UDP ipv6";  // IPv6 UDP protocol
 	}
 }
 
+// Function to perform reverse DNS lookup to get the hostname and service name
+void reverse_lookup(const char *ip, int port) {
+    struct sockaddr_storage sa;  // Structure to hold address information (IPv4/IPv6)
+    char host[1024];  // Buffer to store hostname
+    char service[20];  // Buffer to store service name
+    memset(&sa, 0, sizeof(sa));  // Initialize the address structure to zero
+
+    // Determine if the IP is IPv4 or IPv6 and set up the address structure accordingly
+    if (strchr(ip, ':')) {  // Check for ':' to identify IPv6 address
+        struct sockaddr_in6 *sa6 = (struct sockaddr_in6 *)&sa;
+        sa6->sin6_family = AF_INET6;
+        sa6->sin6_port = htons(port);
+        if (inet_pton(AF_INET6, ip, &sa6->sin6_addr) != 1) {
+            fprintf(stderr, "Invalid IPv6 address: %s\n", ip);
+            return;
+        }
+    } else {  // Otherwise, treat as IPv4 address
+        struct sockaddr_in *sa4 = (struct sockaddr_in *)&sa;
+        sa4->sin_family = AF_INET;
+        sa4->sin_port = htons(port);
+        if (inet_pton(AF_INET, ip, &sa4->sin_addr) != 1) {
+            fprintf(stderr, "Invalid IPv4 address: %s\n", ip);
+            return;
+        }
+    }
+
+    // Perform reverse DNS lookup
+    if (getnameinfo((struct sockaddr *)&sa, sizeof(sa), host, sizeof(host), service, sizeof(service), 0) != 0) {
+        fprintf(stderr, "Could not resolve hostname for IP: %s, Port: %d\n", ip, port);
+        return;
+    }
+
+    // Print the resolved hostname and service
+    printf("Hostname: %s\n", host);
+    if (strlen(service) == 0 || strcmp(service, "0") == 0 || atoi(service) == port) {
+        printf("Service: Unknown\n");  // If service name is empty or matches port, report as unknown
+    } else {
+        printf("Service: %s\n", service);  // Print the resolved service name
+    } 
+}
+
+// Function to increase the memory lock limit (RLIMIT_MEMLOCK)
 static void bump_memlock_rlimit(void)
 {
 	struct rlimit rlim_new = {
@@ -48,26 +92,31 @@ static void bump_memlock_rlimit(void)
 	}
 }
 
+// Signal handler to set the exiting flag
 static void sig_handler(int sig)
 {
 	exiting = true;
 }
 
+// Function to handle events from the BPF ring buffer
 static int handle_evt(void *ctx, void *data, size_t sz)
 {	
-	struct tc_evt *evt = data;
+	struct tc_evt *evt = data;  // Cast the data to the expected event type
 
+	// Print whether the packet is allowed or blocked
 	if(evt->pkt_state == ALLOWED) printf("ALLOWED ");
 	else printf("BLOCKED ");
 
+	// Handle events based on Ethernet type
 	if(evt->eth_type == ETH_P_IP || evt->eth_type == ETH_P_IPV6) {
 		fflush(stdout);
-		
+
+		// Print the command name, thread group ID, and process ID
 		printf("comm %s\n", evt->comm);
 		printf("tgid %d :: pid %d\n", evt->tgid, evt->pid);	
 		
+		// Handle IPv4 and IPv6 packets
 		if(evt->ip.ipp == TCP_V4 || evt->ip.ipp == UDP_V4) {
-			//daddr here
 			char addr[15];
 			memset(addr, 0, sizeof(addr));
 			snprintf(addr, sizeof(addr), "%d.%d.%d.%d",
@@ -76,30 +125,36 @@ static int handle_evt(void *ctx, void *data, size_t sz)
 				evt->ip.addr.ipv4_daddr[2],
 				evt->ip.addr.ipv4_daddr[3]);
 			printf("dest: %s\n", addr);
+
+      reverse_lookup(addr, evt->ip.port);  // Perform reverse lookup for IPv4 address
+
 		} else {
 			printf("dest: ");
-            char addr[30];
-            char a[6];
-            memset(addr, 0, sizeof(addr));
-            for (int i = 0; i < 14; i+=2) {
-                snprintf(a, 6, "%02x%02x:",
-                    evt->ip.addr.ipv6_daddr[i],
-                    evt->ip.addr.ipv6_daddr[i+1]);
-                strncat(addr, a, 6);
-            }
-            snprintf(a, 6, "%02x%02x",
-                evt->ip.addr.ipv6_daddr[14],
-                evt->ip.addr.ipv6_daddr[15]);
-            strncat(addr, a, 6);
-            printf("%s\n", addr);
+      char addr[30];
+      char a[6];
+      memset(addr, 0, sizeof(addr));
+      for (int i = 0; i < 14; i+=2) {
+        snprintf(a, 6, "%02x%02x:",
+                 evt->ip.addr.ipv6_daddr[i],
+                 evt->ip.addr.ipv6_daddr[i+1]);
+        strncat(addr, a, 6);
+      }
+      snprintf(a, 6, "%02x%02x",
+               evt->ip.addr.ipv6_daddr[14],
+               evt->ip.addr.ipv6_daddr[15]);
+      strncat(addr, a, 6);
+      printf("%s\n", addr);
+      
+      reverse_lookup(addr, evt->ip.port);  // Perform reverse lookup for IPv6 address
 		}
 		printf("port: %d\n", evt->ip.port);
-		printf("protocol %s\n", print_proto(evt->ip.ipp));
-	} else {// if evt->eth_type == ETH_P_ARP
+		printf("protocol %s\n", print_proto(evt->ip.ipp));  // Print the protocol type
+	} else {  // Handle ARP packets
 		printf("eth type 0x%04x\n", evt->eth_type);
-		printf("comm: %s\n", evt->comm);//remember that this happens twice which is stupid
+		printf("comm: %s\n", evt->comm);  // Print the command name (appears twice which might be unnecessary)
 		printf("tgid %d :: pid %d\n", evt->tgid, evt->pid);
 
+		// Print ARP packet details
 		printf("hardware: %d\n", evt->arp.ar_hrd);
 		printf("proto: %d\n", evt->arp.ar_pro);
 		printf("len hard: %d\n", evt->arp.ar_hln);
@@ -112,10 +167,11 @@ static int handle_evt(void *ctx, void *data, size_t sz)
 	return 0;
 }
 
+// Function to add a port to the allowed ports map
 void allow_port(int map_fd, uint16_t port)
 {
 	static uint32_t key = 0;
-	bpf_map_update_elem(map_fd, &key, &port, BPF_ANY);
+	bpf_map_update_elem(map_fd, &key, &port, BPF_ANY);  // Update the map with the new port
 	key ++;
 }
 
@@ -123,14 +179,15 @@ int main(int argc, char **argv)
 {
 	DECLARE_LIBBPF_OPTS(bpf_tc_hook, hook, .ifindex = LO_IFINDEX, .attach_point = BPF_TC_EGRESS);
 	DECLARE_LIBBPF_OPTS(bpf_tc_opts, opts, .handle = 1, .priority = 1);
-	bump_memlock_rlimit();
+	bump_memlock_rlimit();  // Increase memory lock limit
 
-	signal(SIGINT, sig_handler);
+	signal(SIGINT, sig_handler);  // Set up signal handlers for graceful exit
 	signal(SIGTERM, sig_handler);
 
-	struct tc *skel = tc__open_and_load();
-	skel->bss->my_pid = getpid();
+	struct tc *skel = tc__open_and_load();  // Load the BPF program
+	skel->bss->my_pid = getpid();  // Set the current process ID in the BPF program
 
+	// Configure and attach the BPF TC program to the egress hook
 	bpf_tc_hook_create(&hook);
 	hook.attach_point = BPF_TC_CUSTOM;
 	hook.parent = TC_H_MAKE(TC_H_CLSACT, TC_H_MIN_EGRESS);
@@ -140,24 +197,28 @@ int main(int argc, char **argv)
 
 	bpf_tc_attach(&hook, &opts);
 
+	// Add allowed ports from command-line arguments to the BPF map
 	int map_fd = bpf_map__fd(skel->maps.ports);
-	for (int i=0; i < argc; i++) {
+	for (int i = 0; i < argc; i++) {
 		int port = atoi(argv[i]);
 		allow_port(map_fd, port);
 	}
 
+	// Create a ring buffer for event handling
 	struct ring_buffer *rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_evt, NULL, NULL);
 	
-	while(!exiting) {
+	// Poll for events until the exit signal is received
+	while (!exiting) {
 		ring_buffer__poll(rb, 1000);
 	}
 
+	// Detach and destroy the BPF TC program
 	opts.flags = opts.prog_id = opts.prog_fd = 0;
     int dtch = bpf_tc_detach(&hook, &opts);
     int dstr = bpf_tc_hook_destroy(&hook);
 
+    // Print the results of detach and destroy operations
     printf("%d -- %d\n", dtch, dstr);
     
     return 0;
-
 }
